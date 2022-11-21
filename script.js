c8740e94a3e86119ac85b9e0e43ab7646ec5289a
D <Dickerson.Morgan@outlook.com>
2022-11-21 14:20:19 -0500

    added boarder around current weather section. added comments to html and js

--- script.js
+++ script.js
@@ -1,2 +1,22 @@
-var city = "";
+// my apikey
 var apikey = "46284fce87c7f71033bf8fc8bd177bd1";
+
+// 
+var inputEl = document.querySelector(".input");
+
+// stores city name in local storage
+var cityName = localStorage.getItem("citynameContent");
+
+// current day weather url
+var currentWeatherURL = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&APPID=" + apikey;
+// five day weather url
+var forcastWeatherURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + cityName + "&APPID=" + apikey;
+
+// 
+function recordCityName () {
+    localStorage.setItem("citynameContent", inputEl.vaule);
+}
+
+for (var i = 0; 1 < localStorage.length; i++) {
+    $()
+}
